45b62e3bacc4c10a35a706630f661b5752d6674b
YuriyEG <you@example.com>
2023-09-14 21:20:17 +0300

    base done

--- src/AppAviasales/AppAviasales.js
+++ src/AppAviasales/AppAviasales.js
@@ -1,13 +1,22 @@
 
 import React from "react";
 import Ticket from '../Ticket';
+import Filter from '../Filter';
+
+
 
 
 
 const AppAviasales = () => {
     return (
         <div className="app-aviasales">
-            <Ticket />
+            
+            <div className="app-aviasales__logo"></div>
+            <Filter />
+            {
+                [1,3,3,4, 34].map( node => <Ticket />)
+            }
+
         </div>
     )
 }
--- src/Ticket/Ticket.js
+++ src/Ticket/Ticket.js
@@ -1,12 +1,34 @@
 import React from "react";
 import './Ticket.css';
+const fakeData = {
+    price: '14 450',
+        route: {
+            header: 'MOW - NKT',
+            time: '10:45 - 8:00'
+        },
+        travelTime: '21ч 15м',
+        tranfer: {
+            transferCount: 2,
+            transferCities: 'NKG FGE'
+        }
+}
+
+const price = fakeData.price;
+const header = fakeData.route.header;
+const time = fakeData.route.time;
+const travelTime = fakeData.travelTime;
+const transferCount = fakeData.tranfer.transferCount;
+const transferCities = fakeData.tranfer.transferCities;
+
 
 const Ticket = () => {
+
+    
     return (
         <div className="ticket">
             <div className="ticket__title">
                 <div className="ticket__price">
-                    13 400 р
+                    {price} Р
                 </div>
                 <div className="ticket__logo">
 
@@ -14,30 +36,30 @@ const Ticket = () => {
             </div>
             <div className="ticket__flight-info">
                 <div className="ticket__route">
-                    <div className="ticket__cities-header">MOW - NKT</div>
-                    <div className="ticket__cities-time">10:45 - 8:00</div>
+                    <div className="ticket__cities-header">{header}</div>
+                    <div className="ticket__cities-time">{time}</div>
                 </div>
                 <div className="ticket__travel-time">
                     <div className="ticket__travel-time-header">В ПУТИ</div>
-                    <div className="ticket__travel-time-info">21ч 15м</div>
+                    <div className="ticket__travel-time-info">{travelTime}</div>
                 </div>
                 <div className="ticket__transfer">
-                    <div className="ticket__transfer-count">2 ПЕРЕСАДКИ</div>
-                    <div className="ticket__transfer-cities">NKG NGE</div>
+                    <div className="ticket__transfer-count">{transferCount} ПЕРЕСАДКИ</div>
+                    <div className="ticket__transfer-cities">{transferCities}</div>
                 </div>
             </div>
             <div className="ticket__flight-info">
                 <div className="ticket__route">
-                    <div className="ticket__cities-header">MOW - NKT</div>
-                    <div className="ticket__cities-time">10:45 - 8:00</div>
+                    <div className="ticket__cities-header">{header}</div>
+                    <div className="ticket__cities-time">{time}</div>
                 </div>
                 <div className="ticket__travel-time">
                     <div className="ticket__travel-time-header">В ПУТИ</div>
-                    <div className="ticket__travel-time-info">21ч 15м</div>
+                    <div className="ticket__travel-time-info">{travelTime}</div>
                 </div>
                 <div className="ticket__transfer">
-                    <div className="ticket__transfer-count">2 ПЕРЕСАДКИ</div>
-                    <div className="ticket__transfer-cities">NKG NGE</div>
+                    <div className="ticket__transfer-count">{transferCount} ПЕРЕСАДКИ</div>
+                    <div className="ticket__transfer-cities">{transferCities}</div>
                 </div>
             </div>
 
