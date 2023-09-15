db78866f1018bcaf566dd2f2efc63e018de95681
YuriyEG <you@example.com>
2023-09-15 07:57:43 +0300

    adaptive started

--- src/AppAviasales/AppAviasales.js
+++ src/AppAviasales/AppAviasales.js
@@ -3,6 +3,7 @@ import React from "react";
 import Ticket from '../Ticket';
 import Filter from '../Filter';
 import FilterOptions from "../FilterOptions";
+import FiveMoreButton from '../FiveMoreButton';
 
 
 
@@ -11,14 +12,15 @@ import FilterOptions from "../FilterOptions";
 const AppAviasales = () => {
     return (
         <div className="app-aviasales">
-            <FilterOptions />
+            
+
             <div className="app-aviasales__logo"></div>
             <Filter />
-            
+            <FilterOptions />
             {
-                [1,3,3,4, 34].map( node => <Ticket />)
+                [1,3,3,4,5].map( node => <Ticket />)
             }
-
+            <FiveMoreButton />
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/AppAviasales';
 import AppAviasales from '../src/AppAviasales';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
